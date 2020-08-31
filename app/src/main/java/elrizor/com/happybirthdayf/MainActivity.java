585950eeea158a7fc430019ab624f7e52c7aa9fd
rauluis <lrs18110328@purisima.tecnm.mx>
2020-08-31 16:36:59 -0500

    Initial commit

---
+++ app/src/main/java/elrizor/com/happybirthdayf/MainActivity.java
@@ -0,0 +1,14 @@
+package elrizor.com.happybirthdayf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
